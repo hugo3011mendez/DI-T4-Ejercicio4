2e7fa68e704ae76ee1d56204758608b47e723879
Hugo <hugo3011mendez@gmail.com>
2020-12-10 13:18:09 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace Ejercicio4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOperacion = new System.Windows.Forms.Button();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.rbSuma = new System.Windows.Forms.RadioButton();
+            this.rbResta = new System.Windows.Forms.RadioButton();
+            this.rbMultiplicacion = new System.Windows.Forms.RadioButton();
+            this.rbDivision = new System.Windows.Forms.RadioButton();
+            this.lblSigno = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOperacion
+            // 
+            this.btnOperacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.btnOperacion.Location = new System.Drawing.Point(483, 60);
+            this.btnOperacion.Name = "btnOperacion";
+            this.btnOperacion.Size = new System.Drawing.Size(75, 31);
+            this.btnOperacion.TabIndex = 0;
+            this.btnOperacion.Text = "=";
+            this.btnOperacion.UseVisualStyleBackColor = true;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtNum1.Location = new System.Drawing.Point(81, 60);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(132, 29);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.txtNum2.Location = new System.Drawing.Point(306, 60);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(132, 29);
+            this.txtNum2.TabIndex = 2;
+            // 
+            // rbSuma
+            // 
+            this.rbSuma.AutoSize = true;
+            this.rbSuma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbSuma.Location = new System.Drawing.Point(47, 124);
+            this.rbSuma.Name = "rbSuma";
+            this.rbSuma.Size = new System.Drawing.Size(69, 24);
+            this.rbSuma.TabIndex = 3;
+            this.rbSuma.TabStop = true;
+            this.rbSuma.Text = "Suma";
+            this.rbSuma.UseVisualStyleBackColor = true;
+            // 
+            // rbResta
+            // 
+            this.rbResta.AutoSize = true;
+            this.rbResta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbResta.Location = new System.Drawing.Point(46, 170);
+            this.rbResta.Name = "rbResta";
+            this.rbResta.Size = new System.Drawing.Size(70, 24);
+            this.rbResta.TabIndex = 4;
+            this.rbResta.TabStop = true;
+            this.rbResta.Text = "Resta";
+            this.rbResta.UseVisualStyleBackColor = true;
+            // 
+            // rbMultiplicacion
+            // 
+            this.rbMultiplicacion.AutoSize = true;
+            this.rbMultiplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbMultiplicacion.Location = new System.Drawing.Point(143, 124);
+            this.rbMultiplicacion.Name = "rbMultiplicacion";
+            this.rbMultiplicacion.Size = new System.Drawing.Size(121, 24);
+            this.rbMultiplicacion.TabIndex = 5;
+            this.rbMultiplicacion.TabStop = true;
+            this.rbMultiplicacion.Text = "Multiplicación";
+            this.rbMultiplicacion.UseVisualStyleBackColor = true;
+            // 
+            // rbDivision
+            // 
+            this.rbDivision.AutoSize = true;
+            this.rbDivision.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.rbDivision.Location = new System.Drawing.Point(143, 170);
+            this.rbDivision.Name = "rbDivision";
+            this.rbDivision.Size = new System.Drawing.Size(81, 24);
+            this.rbDivision.TabIndex = 6;
+            this.rbDivision.TabStop = true;
+            this.rbDivision.Text = "División";
+            this.rbDivision.UseVisualStyleBackColor = true;
+            // 
+            // lblSigno
+            // 
+            this.lblSigno.AutoSize = true;
+            this.lblSigno.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblSigno.Location = new System.Drawing.Point(229, 63);
+            this.lblSigno.Name = "lblSigno";
+            this.lblSigno.Size = new System.Drawing.Size(59, 24);
+            this.lblSigno.TabIndex = 7;
+            this.lblSigno.Text = "Signo";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.lblResultado.Location = new System.Drawing.Point(597, 63);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(94, 24);
+            this.lblResultado.TabIndex = 8;
+            this.lblResultado.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 225);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblSigno);
+            this.Controls.Add(this.rbDivision);
+            this.Controls.Add(this.rbMultiplicacion);
+            this.Controls.Add(this.rbResta);
+            this.Controls.Add(this.rbSuma);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.btnOperacion);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperacion;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.RadioButton rbSuma;
+        private System.Windows.Forms.RadioButton rbResta;
+        private System.Windows.Forms.RadioButton rbMultiplicacion;
+        private System.Windows.Forms.RadioButton rbDivision;
+        private System.Windows.Forms.Label lblSigno;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ Ejercicio4/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4
+{
+    public partial class Form1 : Form
+    {
+        // Creo y declaro el delegado y la Hashtable
+        public delegate double Operacion(double num1, double num2);
+        Hashtable operaciones = new Hashtable();
+
+        // Procedo a crear los diferentes delegados Operacion con funciones lambda indicando cada operación aritmética necesaria :
+        Operacion suma = new Operacion((num1, num2) =>
+        {
+            return num1 + num2;
+        });
+
+        Operacion resta = new Operacion((num1, num2) =>
+        {
+            return num1 - num2;
+        });
+
+        Operacion multiplicacion = new Operacion((num1, num2) =>
+        {
+            return num1 * num2;
+        });
+
+        Operacion division = new Operacion((num1, num2) =>
+        {
+            return num1 / num2;
+        });
+
+        Operacion seleccionada;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            lblSigno.Text = "";
+            lblResultado.Text = "";
+
+            // Añado los delegados a la Hashtable
+            operaciones.Add("Suma", suma);
+            operaciones.Add("Resta", resta);
+            operaciones.Add("Multiplicación", multiplicacion);
+            operaciones.Add("División", division);
+        }
+
+        public void pulsarRadioButton(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
